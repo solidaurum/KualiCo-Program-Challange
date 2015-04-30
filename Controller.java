605b46f16eca67641e208e2344d59bab6e5b3a43
solidaurum <solidaurum@users.noreply.github.com>
2015-04-30 17:33:07 -0500

    Defining class attributes

--- Controller.java
+++ Controller.java
@@ -4,10 +4,24 @@
  */
 package elevatorsimulation;
 
+import java.util.Queue;
+
 /**
  *
  * @author Ammon
  */
 public class Controller {
+    //Array to store status of elevators in building
+    private Elevator[] elevators;
+    
+    //int to set upper boundry for floors in building
+    private int buildingFloors;
+    
+    //Array of request objects (1 for each floor)
+    private Request[] requests;
+    
+    //Queue to track call requests
+    private Queue<requests> callQueue;
+    
     
 }
