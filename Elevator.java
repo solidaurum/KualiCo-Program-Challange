58cbb124d67fc3fcfebddac74caee3bb26ffb5d9
solidaurum <solidaurum@users.noreply.github.com>
2015-04-30 17:16:43 -0500

    Initial Class Creation
    After requirements analysis these classes have been made to represent
    objects.

---
+++ Elevator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elevatorsimulation;
+
+/**
+ *
+ * @author Ammon
+ */
+public class Elevator {
+    
+}
