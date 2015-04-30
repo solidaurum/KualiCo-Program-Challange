15adc6ea6c10f7825f00520d2234965b75e94ff3
solidaurum <solidaurum@users.noreply.github.com>
2015-04-30 18:03:09 -0500

    Development of Controller

--- Controller.java
+++ Controller.java
@@ -14,6 +14,9 @@ public class Controller {
     //Array to store status of elevators in building
     private Elevator[] elevators;
     
+    //Elevators to add to array
+    private Elevator elevator;
+    
     //int to set upper boundry for floors in building
     private int buildingFloors;
     
@@ -21,7 +24,39 @@ public class Controller {
     private Request[] requests;
     
     //Queue to track call requests
-    private Queue<requests> callQueue;
+    private Queue<Request> callQueue;
+
+    /*
+     * Constructor for Controller shell
+     * int buildingFloors : number of floors in building
+     * int numberElevators: number of elevators in building
+     */
+    public Controller(int buildingFloors, int numberElevators) {
+        this.buildingFloors = buildingFloors;
+        elevators = new Elevator[numberElevators];
+        requests = new Request[buildingFloors];
+        populateElevators();
+        populateRequestStations();
+    }
     
+    /*
+     * Populates elevators for building
+     */
+    private void populateElevators(){
     
+        for(int i = 0; i < elevators.length; i++){
+            elevator = new Elevator();
+            elevators[i] = elevator;
+        }
+    }
+    
+    /*
+     * populates request stations
+     */
+    private void populateRequestStations(){
+    
+        for(int i = 0; i < requests.length; i++){
+            
+        }
+    }
 }
